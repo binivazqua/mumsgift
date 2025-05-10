5661a6dfd7ce97c401e34352619ab55f7fae9ec0
binivazqua <binizavazquez@gmail.com>
2025-05-09 21:11:46 -0600

    tqm mami

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-pink-100 text-center p-10">
+      <h1 className="text-4xl font-bold text-pink-600">
+        ¡Feliz Día de las Madres! 💐
+      </h1>
+    </div>
+  );
+}
+export default App;
